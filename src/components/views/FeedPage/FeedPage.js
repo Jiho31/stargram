e8ad7c0ec70a65258b1c0f4a267248ba394a1bc2
Ji Ho Bok <bok.jiho@gmail.com>
2020-12-09 00:12:35 +0900

    [feat] Add UI of navigation bar
    동작 제외하고 정적인 부분만 구현

--- src/components/views/FeedPage/FeedPage.js
+++ src/components/views/FeedPage/FeedPage.js
@@ -1,18 +1,47 @@
 import React, {Component} from 'react';
 import './FeedPage.css';
 
+class HomeButton extends Component {
+    render() {
+        return (
+            <img src="https://instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+                style={{ width: '103px', height: '29px', display: 'block' }}
+                alt="homebutton" />
+        );
+    }
+}
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <input className="search-input"
+                placeholder="검색" />
+        );
+    }
+}
+
+class NavIcons extends Component {
+    render() {
+        return (
+            <div className="nav-icons">
+
+            </div>
+        );
+    }
+}
+
 class FeedNav extends Component {
     render() {
         return (
             <nav className="nav-bar">
                 <section>
-                    
+                    <HomeButton />
                 </section>
                 <section>
-
+                    <SearchBar />
                 </section>
                 <section>
-
+                    <NavIcons />
                 </section>
             </nav>
         );
@@ -22,14 +51,14 @@ class FeedNav extends Component {
 class FeedBody extends Component {
     render() {
         return (
-            <>
+            <div className="feed-body">
                 <main>
-                    hiiiiiii
+                    
                 </main>
                 <aside>
         
                 </aside>
-            </>
+            </div>
         );
     }
 }
