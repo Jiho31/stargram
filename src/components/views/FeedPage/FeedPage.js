30390a2d7ca13aab4f49f3102e82cbb9a2428e65
Ji Ho Bok <bok.jiho@gmail.com>
2020-12-10 00:18:29 +0900

    [feat] Add UI of story box

--- src/components/views/FeedPage/FeedPage.js
+++ src/components/views/FeedPage/FeedPage.js
@@ -48,16 +48,61 @@ class FeedNav extends Component {
     }
 }
 
+class StoryContent extends Component {
+    // const profileImagePath = this.props.uimagepath;
+
+    render() {
+        return (
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '80px', padding: '0 4px' }}>
+                <div className="story-circle">
+                    <img className="story-profile-image"
+                        src={this.props.profileImagePath} 
+                        alt="" />
+                </div>
+                <div className="story-user-id">
+                    {this.props.uid}
+                </div>
+            </div>
+        );
+    }
+}
+
+class MainBody extends Component {
+    render() {
+        const imagePath = "https://scontent-gmp1-1.cdninstagram.com/v/t51.2885-19/s320x320/123286692_636049017275345_9161752167566283993_n.jpg?_nc_ht=scontent-gmp1-1.cdninstagram.com&_nc_ohc=6T2wLw9gL5oAX99vxLM&tp=1&oh=3f6d12f8114066dc9787b17a76b2e459&oe=5FFBA2AA";
+
+        return (
+            <main className="main-body">
+                <section className="story-box">
+                    <StoryContent uid="diptyque" 
+                        profileImagePath={imagePath} />
+                    <StoryContent uid="zl.ll07" 
+                        profileImagePath={imagePath} />
+                </section>
+                <section className="feed-content">
+
+                </section>
+            </main>
+        );
+    }
+}
+
+class SideBody extends Component {
+    render() {
+        return (
+            <aside className="side-body">
+
+            </aside>
+        );
+    }
+}
+
 class FeedBody extends Component {
     render() {
         return (
             <div className="feed-body">
-                <main>
-                    
-                </main>
-                <aside>
-        
-                </aside>
+                <MainBody />
+                <SideBody />
             </div>
         );
     }
