c9bd104ab4ad44ca64b137757a2ff81e3e96eed8
Ji Ho Bok <bok.jiho@gmail.com>
2021-01-20 19:16:13 +0900

    [remove] Remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -4,15 +4,6 @@ import LandingPage from './components/views/LandingPage/LandingPage';
 import FeedPage from './components/views/FeedPage/FeedPage';
 
 class App extends Component {
-  state = {
-    count: 0,
-  };
-
-  handleChange = () => {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  };
 
   render() {
     return (
