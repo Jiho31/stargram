24d81bbc5e6ae3984c6fe820e23e6c02b2ffd2e6
Jiho Bok <32476867+Jiho31@users.noreply.github.com>
2021-09-23 19:47:25 +0900

    [remove] Remove unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import LandingPage from './components/views/LandingPage/LandingPage';
-import FeedPage from './components/views/FeedPage/FeedPage';
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import FeedPage from "./components/views/FeedPage/FeedPage";
 
-class App extends Component {
-
-  render() {
-    return (
-      <BrowserRouter>
-        <div className="App">
-          <Route path="/home" component={LandingPage}></Route>
-          <Route path="/feed" component={FeedPage}></Route>
-        </div>
-      </BrowserRouter>
-    );
-  }
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Route path="/home" component={LandingPage}></Route>
+        <Route path="/feed" component={FeedPage}></Route>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,70 +1,10 @@
-import React, { useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-/*
-class EssayForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 'Please write an essay about your favorite DOM element.'
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('An essay was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Essay:
-          <textarea value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
-  }
-}
-
-function Practice() {
-
-  useEffect(() => {
-    document.getElementById('submit-button').addEventListener('click', e => {
-      e.preventDefault();
-      alert('submitted');
-    });
-  });
-  
-
-  return (
-    <>
-      <form method="POST" action="" enctype="multipart/form-data">
-        <div>
-          <label for="profile-pic">Choose a file</label>
-          <input type="file" id="profile-pic" name="profilepic" />
-        </div>
-        <div>
-          <input type="submit" id="submit-button" value="Submit" />
-        </div>
-      </form>
-    </>
-  );
-}
-*/
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
