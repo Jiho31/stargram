9cb64008bbd67df16261fb44eb688e798d1c6387
Jiho Bok <32476867+Jiho31@users.noreply.github.com>
2021-10-26 15:09:57 +0900

    Updates

---
+++ precache-manifest.078318b176b50715f5f5845253a0257d.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f71e0bbbeafbb21b0a0b572f88d738b3",
+    "url": "/stargram/index.html"
+  },
+  {
+    "revision": "3e0abb8fdf11333b92a6",
+    "url": "/stargram/static/css/main.700a2ec6.chunk.css"
+  },
+  {
+    "revision": "184231fef6b3c5a99ced",
+    "url": "/stargram/static/js/2.8fa3a647.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/stargram/static/js/2.8fa3a647.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e0abb8fdf11333b92a6",
+    "url": "/stargram/static/js/main.f1299974.chunk.js"
+  },
+  {
+    "revision": "0abbf2e1bf928bf49146",
+    "url": "/stargram/static/js/runtime-main.2cbdebd1.js"
+  },
+  {
+    "revision": "1afba2345ff66df3ab89154029a6dfcd",
+    "url": "/stargram/static/media/post1.1afba234.jpg"
+  },
+  {
+    "revision": "1053fbd64c88d6463a86b6643040ed9f",
+    "url": "/stargram/static/media/post10 (2).1053fbd6.jpg"
+  },
+  {
+    "revision": "b82f4564fca7c97dbbd60d51fcb4fc07",
+    "url": "/stargram/static/media/post10 (3).b82f4564.jpg"
+  },
+  {
+    "revision": "453c8bd4ac26ac5340ab45196da29497",
+    "url": "/stargram/static/media/post10.453c8bd4.jpg"
+  },
+  {
+    "revision": "3cb7db02c69a87db66cf8c71ada743d4",
+    "url": "/stargram/static/media/post11 (2).3cb7db02.jpg"
+  },
+  {
+    "revision": "af5cf38ce68d542a392b7d0d1ccdeffc",
+    "url": "/stargram/static/media/post11.af5cf38c.jpg"
+  },
+  {
+    "revision": "578711d699a501a75d3b547b6b0277f5",
+    "url": "/stargram/static/media/post12.578711d6.jpg"
+  },
+  {
+    "revision": "a3e94dd0df6ae3d0639f1dbb7c2ac09f",
+    "url": "/stargram/static/media/post13.a3e94dd0.jpg"
+  },
+  {
+    "revision": "203ca18569cb03f35f587d826c446a29",
+    "url": "/stargram/static/media/post14.203ca185.jpg"
+  },
+  {
+    "revision": "a807deb334142af4330e3603c3dca781",
+    "url": "/stargram/static/media/post15.a807deb3.jpg"
+  },
+  {
+    "revision": "b9d49343e2259bfa44c638d8d8e63511",
+    "url": "/stargram/static/media/post2.b9d49343.jpg"
+  },
+  {
+    "revision": "ccda637e42e73f64f9d10d0f377a6b80",
+    "url": "/stargram/static/media/post3 (2).ccda637e.jpg"
+  },
+  {
+    "revision": "67eb65204c260db61c1575b2e0ef6f61",
+    "url": "/stargram/static/media/post3 (3).67eb6520.jpg"
+  },
+  {
+    "revision": "794ad3fbb92e21fda415be003c715cdc",
+    "url": "/stargram/static/media/post3.794ad3fb.jpg"
+  },
+  {
+    "revision": "e9f451ffe7b7ca6204237cc4baadd16a",
+    "url": "/stargram/static/media/post4.e9f451ff.jpg"
+  },
+  {
+    "revision": "3650de677643b1c0048bbdcaa08a6151",
+    "url": "/stargram/static/media/post5.3650de67.jpg"
+  },
+  {
+    "revision": "3322812703ca635de2b868deda3f61bc",
+    "url": "/stargram/static/media/post6.33228127.jpg"
+  },
+  {
+    "revision": "77ae8dd8fbb0eaf3a90232e952a63836",
+    "url": "/stargram/static/media/post7.77ae8dd8.jpg"
+  },
+  {
+    "revision": "24c86aea32c0b27c47c5ae5330349bce",
+    "url": "/stargram/static/media/post8.24c86aea.jpg"
+  },
+  {
+    "revision": "b2e4380052d647a82943188926ee19c4",
+    "url": "/stargram/static/media/post9.b2e43800.jpg"
+  },
+  {
+    "revision": "f23001133e7cf80527ac60fba78b779a",
+    "url": "/stargram/static/media/profile_bokjiho.f2300113.jpg"
+  },
+  {
+    "revision": "a04d849cf591c2f980548b982f461401",
+    "url": "/stargram/static/media/profile_default.a04d849c.jpg"
+  },
+  {
+    "revision": "bf8771fe929b435e01272d82b71896ec",
+    "url": "/stargram/static/media/profile_drmartenskr.bf8771fe.jpg"
+  },
+  {
+    "revision": "e99099fe856a2a000acf4c88919d434d",
+    "url": "/stargram/static/media/profile_skawngur.e99099fe.jpg"
+  }
+]);
