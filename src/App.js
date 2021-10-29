90f5bfc59640f12782934d140885bef9e4804209
Jiho Bok <32476867+Jiho31@users.noreply.github.com>
2021-10-29 17:56:01 +0900

    [Remove] Delete unusued code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import LandingPage from "./views/LandingPage";
 import FeedPage from "./views/FeedPage";
 
--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,5 +1,4 @@
-import React, { Component, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { Component, useState } from "react";
 import "./LandingPage.css";
 
 const userLoginInfo = {
